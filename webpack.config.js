a2ad0215eba98edf5b62678900fa475a475b37d8
Mit9l <jluzya@gmail.com>
2016-06-15 18:37:08 +0300

    stop on middleware

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var NpmInstallPlugin  = require('npm-install-webpack-plugin');
 var path  = require('path') 
 var webpack = require('webpack')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss')
 
 module.exports  = {   
   devtool:  'cheap-module-eval-source-map',   
@@ -40,7 +42,14 @@ module.exports  = {
         ],                
         test: /\.js$/,                
         plugins:  ['transform-runtime'],            
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
       }       
     ]   
-  } 
+  },
+  postcss: function() {
+    return [autoprefixer, precss]
+  }
 }
